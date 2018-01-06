5744d036ca471e16e6d754b2997c11c32a292318
ChrisHolc <chris_holc@hotmail.fr>
2018-01-06 16:29:14 +0100

    Added routes in separate file + added events

--- server/models/event.js
+++ server/models/event.js
@@ -1,22 +1,21 @@
 const mongoose = require('mongoose');
 
-const User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  },
-  username: {
-    type: String,
-    required: true,
-    minlength: 1
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 1
-  }
+const Event = mongoose.model('Event', {
+    start_date: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    end_date: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    text: {
+        type: String,
+        required: true,
+        minlength: 1
+    }
 });
 
-module.exports = { User };
+module.exports = {Event};
--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -1,22 +1,45 @@
 let express = require('express');
 let userRouter = express.Router();
-let userDao = require('../dao/user-dao.js');
+const { User } = require('../models/user');
 
-userRouter.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+userRouter.post('/', (req, res) => {
+    let userToSave = new User({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+    userToSave.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-//connection, return null if there is no match
-userRouter.post('/login', function (req, res) {
-    userDao.findUserByUsernameAndPassword(req.body.username, req.body.password, function (err, rows) {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(rows);
-        }
-    })
+userRouter.get('/', (req, res) => {
+
+    User.find().then(users => {
+        res.send({ users });
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+});
+
+userRouter.get('/:id', (req, res) => {
+    let { id } = req.params;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    else {
+        User.findById(id).then(user => {
+            if (!user) {
+                return res.status(404).send();
+            }
+            res.send({ user });
+        }).catch(e => {
+            res.status(400).send();
+        });
+    }
 });
 
 module.exports = userRouter;
